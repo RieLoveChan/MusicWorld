fe2997901fc83216203962f2cd20357c1ae9f3b5
RieLoveChan <32554439+RieLoveChan@users.noreply.github.com>
2025-06-01 12:07:35 -0400

    Delete conf.lua

--- conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-    t.window.width = 1920
-    t.window.height = 1080
-    t.window.title = "Rhythm Game (Conf)" -- Title from conf
-    t.modules.joystick = true
-    t.modules.physics = false -- Disable physics if not used
-end
